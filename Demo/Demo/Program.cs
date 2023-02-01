5734cbb1320e91f1cd36fbeabd7720848f4088bd
ronitshinde <rony.shinde753@gmail.com>
2023-02-01 09:32:21 +0530

    [Ronit] add : UC1

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC1 Myobj = new UC1();
+            Myobj.MyMethodUC2();
+            UC1.MyMethodUC1();
+        }
+
+
+    }
+}
